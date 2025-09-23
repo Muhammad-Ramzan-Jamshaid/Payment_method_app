2fdb42a48a778df45308d2ff749148d60f895d9c
Muhammad-Ramzan-Jamshaid <jamshaidali8u@gmail.com>
2025-09-23 12:59:00 +0500

    Adding persiddiond and roles

--- database/seeders/PermissionsAndRolesSeeder.php
+++ database/seeders/PermissionsAndRolesSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +16,54 @@ class PermissionsAndRolesSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $permissions =[
+            'users.view',
+            'users.create',
+            'users.edit',
+            'users.delete',
+            'roles.view',
+            'roles.assign',
+            'payments.view',
+            'payments.create',
+            'payments.refund',
+            'reports.view',
+            'settings.manage'
+        ];
+
+        foreach($permissions as $permission){
+          Permission::firstOrCreate([
+           'name'=>$permission
+          ]);
+        }
+
+        // adding roles
+        
+        $superAdmin = Role::firstOrCreate(['name'=> 'super-admin']);
+        $admin = Role::firstOrCreate(['name'=> 'admin']);
+        $user = Role::firstOrCreate(['name'=> 'user']);
+
+
+        // adding permissions
+
+        $superAdmin->givePermissionTo(Permission::all());
+
+        $admin->givePermissionTo([
+            'users.view',
+            'users.create',
+            'users.edit',
+            'roles.view',
+            'roles.assign',
+            'payments.view',
+            'payments.create',
+            'reports.view'
+        ]);
+
+
+        $user->givePermissionTo([
+          'payments.view','payments.create'
+        ]);
+
+    
+
     }
 }
